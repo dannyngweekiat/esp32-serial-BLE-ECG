4f7ac68af3604e1cb96582d77440fa27fbf8ff4c
Danny Ng <20371760+dannyngweekiat@users.noreply.github.com>
2024-08-04 10:56:22 +0800

    chore: Update main.cpp to show raw result in BLE device

--- src/main.cpp
+++ src/main.cpp
@@ -46,9 +46,9 @@ FIR notchFilter(notchFilterCoefficients);
 #define BLE_DEVICE_NAME "ECG Serial BLE"
 
 // Enable only 1 out of the following 3 options
-#define SHOW_NOTCH_FILTER_RESULT
+// #define SHOW_NOTCH_FILTER_RESULT
 // #define SHOW_MOVING_AVERAGE_RESULT
-// #define SHOW_RAW_RESULT
+#define SHOW_RAW_RESULT
 
 #define SERVICE_UUID "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
